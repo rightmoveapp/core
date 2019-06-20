3ff34530960b45865385b6cba73ef08a94d6d02d
Kasie <kchaplick92@gmail.com>
2019-06-20 17:00:07 -0400

    created views for group and users

--- right_move/api/views.py
+++ right_move/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework.viewsets import ModelViewSet
+from right_move.api.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class userViewSet(ModelViewSet):
+  # API endpoint that allows users to be views or edited
+  queryset = User.objects.all().order_by('-date_joined')
+  serializer_class = UserSerializer
+
+class GroupViewSet(ModelViewSet):
+  queryset = Group.objects.all()
+  serializer_class = GroupSerializer
