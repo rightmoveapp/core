21928f58e7f7bd56bc2082be606b80d5ab619fbf
Kasie <kchaplick92@gmail.com>
2019-06-28 17:44:35 -0400

    models for choices, User attr, and jobs

--- right_move/api/models.py
+++ right_move/api/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
+  category_name = models.CharField(max_length=100)
+
+class Subcategory(models.Model):
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
+  subcategory_name = models.CharField(max_length=100)
+
 class Question(models.Model):
   created_at = models.DateTimeField(auto_now_add = True)
   updated_at = models.DateTimeField(auto_now = True)
   body = models.TextField()
-  category = models.CharField(max_length=100)
-  subcategory = models.CharField(max_length=100)
+  category = models.ForeignKey(Category)
+  subcategory = models.ForeignKey(Subcategory)
+
+class Choices(models.Model):
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
+  choice = models.TextField()
+  question_id = models.ForeignKey(Subcategory)
+
+class User_Attributes(models.Model):
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
+
+class Jobs(models.Model):
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
+  
