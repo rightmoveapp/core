e56bd53954b7e411b4f28f7bbda3f34c5aca9240
Kasie <kchaplick92@gmail.com>
2019-07-04 19:49:06 -0400

    adding cors and fixing auth headers on call

--- right_move/right_move/settings.py
+++ right_move/right_move/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,12 +57,14 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    ##TODO: this is a hole that needs to be patched
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000","https://redstapler.app",]
 
 ROOT_URLCONF = 'right_move.urls'
 
