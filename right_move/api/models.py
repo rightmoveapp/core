248155fb11e12787439a31055ecb73758216bf14
Kasie <kchaplick92@gmail.com>
2019-07-08 13:53:31 -0400

    added a filter to the endpooint so that suers will only receive questions they have not answered before

--- right_move/api/models.py
+++ right_move/api/models.py
@@ -6,7 +6,6 @@ class Category(models.Model):
   created_at = models.DateTimeField(auto_now_add = True)
   updated_at = models.DateTimeField(auto_now = True)
   category_name = models.CharField(max_length=100)
-  sir = models.CharField(max_length=100)
   class Meta:
         verbose_name_plural = "Categories"
 
